94183946aa88e4ee3d13cda364b8ee15a6671529
Nguyễn Thành Đạo <110709579+thanhdaonguyen@users.noreply.github.com>
2024-03-12 15:42:27 +0700

    Refactor loginController.js for improved readability and code formatting

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -5,17 +5,14 @@ const loggingIn = wrapper(async (req, res) => {
   const { username, password } = req.body;
 
   const result = await pool.query(
-    "select * from users where username = $1 and password = $2;",
-    [username, password]
+    "select * from users where username = $1 and password = $2",[username, password]
   );
   if (result.rows.length == 0) {
     return res
       .status(400)
       .json({ message: "username or password is incorrect" });
   } else {
-    res
-      .status(200)
-      .json({ message: "login successfully", data: result.rows[0] });
+    res.status(200).json({ message: "login successfully", data: result.rows[0] });
   }
 });
 
