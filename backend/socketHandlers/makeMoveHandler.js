94ccdada287dca189134987616988bd07602c3b0
nmhung1294 <144262235+nmhung1294@users.noreply.github.com>
2024-03-19 15:03:53 +0700

    Temp

--- backend/socketHandlers/makeMoveHandler.js
+++ backend/socketHandlers/makeMoveHandler.js
@@ -1,5 +1,20 @@
-export default function handleMakeMove(socket, io) {
-    socket.on('make move', () => {
-        console.log('Move made in room: ' + socket.id);
+export default function handleMakeMove(socket, io, roomData) {
+    socket.on('nextMove', (player_in_turn, X_timeCount, O_timeCount, roomCode) => {
+            let room = roomData.get(roomCode);
+            if (room) {
+                if (player_in_turn === room.player1_username) {
+                    let time = X_timeCount;
+                    room.player1_timer = time;
+                    io.to(roomCode).emit('nextMove', {player_in_turn, time: time});
+
+                }
+                else {
+                    let time = O_timeCount;
+                    room.player2_timer = time;
+                    io.to(roomCode).emit('nextMove', {player_in_turn, time: time});
+                }
+            }
     });
 };
+
+
