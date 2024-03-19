57358fb00e078b3aaa066a24f929f3746c100365
Nguyễn Thành Đạo <110709579+thanhdaonguyen@users.noreply.github.com>
2024-03-19 19:07:29 +0700

    install jwt

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,18 +1,30 @@
 import { wrapper } from "../middleware/wrapper.js";
 import pool from "../config/db.js";
+import jwt from "jsonwebtoken";
 
 const loggingIn = wrapper(async (req, res) => {
   const { username, password } = req.body;
 
   const result = await pool.query(
-    "select * from users where username = $1 and password = $2",[username, password]
+    "select * from users where username = $1 and password = $2",
+    [username, password]
   );
   if (result.rows.length == 0) {
     return res
       .status(400)
       .json({ message: "username or password is incorrect" });
   } else {
-    res.status(200).json({ message: "login successfully", data: result.rows[0] });
+    jwt.sign(result.rows[0], "privatekey", { expiresIn: "1h" }, (err, token) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(token);
+    });
+
+
+    res
+      .status(200)
+      .json({ message: "login successfully", data: result.rows[0] });
   }
 });
 
